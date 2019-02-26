486a43ccbabc5894bfbc533564ea8c5b6721c239
Frank Otto <traktofon@gmail.com>
2019-02-26 13:44:20 +0000

    make connection more resilient

--- client/setup.sh
+++ client/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -r etc/tunnel /etc/
+cp -r --preserve=mode etc/tunnel /etc/
 cp etc/systemd/system/*.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable open-tunnel
